683bb56e8e744a27549fb2b47ea769adc5594cfb
TAC <tacwork@server>
2021-12-15 20:35:33 -0500

    add bn256 files

---
+++ ztac_jni/javalib/src/main/java/com/tacplatform/ztac/bn256/Groth16.java
@@ -0,0 +1,10 @@
+package com.tacplatform.ztac.bn256;
+
+public class Groth16 {
+
+    public static native boolean verify(byte[] vk, byte[] proof, byte[] inputs);
+
+    static {
+        new JNILibrary("ztac_jni", Groth16.class).load();
+    }
+}
